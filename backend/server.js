1a8c90a23e404860eca586f4888eaf8a7fafa567
Alexander Moline <alexmoline02@gmail.com>
2025-02-14 22:20:02 -0600

    chore: added postgres and cors dependencies

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,14 @@
 import express from 'express';
 const app = express();
 const port = 3000;
+import cors from "cors";
+
+//middleware
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Server is ready");
-});
+}); 
 
 app.listen(port, () => {
     console.log("Server started at http://localhost:3000" );
