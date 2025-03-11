496ad90dfa3cb7090f807df2bdad7e02624d9d64
Jane Guilbeau <janeguilbeau8@gmail.com>
2025-03-11 17:32:15 -0500

    Created placeholder components for each page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { FaqComponent } from './faq/faq.component';
+import { SearchComponent } from './search/search.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
     {
@@ -12,5 +15,20 @@ export const routes: Routes = [
         path: 'about',
         component: AboutComponent,
         title: 'CAMUP - About Us'
+    },
+    {
+        path: 'faq',
+        component: FaqComponent,
+        title: 'CAMUP - Frequently Asked Questions'
+    },
+    {
+        path: 'search',
+        component: SearchComponent,
+        title: 'CAMUP - Search'
+    },
+    {
+        path: 'contact',
+        component: ContactComponent,
+        title: 'CAMUP - Contact'
     }
 ];
