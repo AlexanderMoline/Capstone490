d6a24138c7241754da2fbe95f03a45448c2ad231
Jane Guilbeau <janeguilbeau8@gmail.com>
2025-03-12 19:40:58 -0500

    Removed Angular Material (can add back later if needed)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle'
-import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-home',
-  imports: [MatSlideToggleModule, MatSelectModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
