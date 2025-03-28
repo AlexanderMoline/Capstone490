0228af4da479b20d06c12990a2b0646bb0aa8b99
Jane Guilbeau <janeguilbeau8@gmail.com>
2025-03-28 17:14:23 -0500

    Created HTML for detail view

--- src/app/detail-view/detail-view.component.ts
+++ src/app/detail-view/detail-view.component.ts
@@ -1,8 +1,10 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-detail-view',
-  imports: [],
+  imports: [ NgOptimizedImage, RouterLink ],
   templateUrl: './detail-view.component.html',
   styleUrl: './detail-view.component.css'
 })
