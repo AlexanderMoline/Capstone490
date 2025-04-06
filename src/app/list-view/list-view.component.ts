13d5f20842cf2357b69a455df9d826f7c3a24bec
Jane Guilbeau <janeguilbeau8@gmail.com>
2025-04-06 13:38:22 -0500

    Added button to list view that retrieves data from backend database

--- src/app/list-view/list-view.component.ts
+++ src/app/list-view/list-view.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { DataService } from '../data.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-list-view',
-  imports: [ RouterLink ],
+  imports: [ RouterLink, NgFor ],
   templateUrl: './list-view.component.html',
   styleUrl: './list-view.component.css'
 })
 export class ListViewComponent {
+  missing_persons: any[] = [];
 
+  constructor(private dataService: DataService) { }
+
+  onClick() {
+    this.dataService.getData().subscribe(
+      data => this.missing_persons = data,
+      error => console.error('Error fetching data:', error)
+    );
+  }
 }
