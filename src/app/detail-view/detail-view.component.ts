5a6abf97d83f1392200bbcf791e7ad0b37d3b10c
Alexander Moline <alexmoline02@gmail.com>
2025-05-01 18:25:51 -0500

    Added info to detail view

--- src/app/detail-view/detail-view.component.ts
+++ src/app/detail-view/detail-view.component.ts
@@ -2,22 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BackendService } from '../backend.service';
 import { RouterLink } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-detail-view',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './detail-view.component.html',
-  styleUrl: './detail-view.component.css'
+  styleUrl: './detail-view.component.css',
 })
 export class DetailViewComponent implements OnInit {
-  caseDetails: any;  // Will store data from the API
+  caseDetails: any; // Will store data from the API
   table: string | null = null;
 
   constructor(
     private backendService: BackendService,
-    private route: ActivatedRoute
-  ) { }
+    private route: ActivatedRoute,
+  ) {}
 
   ngOnInit(): void {
     const caseNumber = this.route.snapshot.paramMap.get('case_number');
@@ -25,8 +25,8 @@ export class DetailViewComponent implements OnInit {
 
     if (caseNumber) {
       this.backendService.getCaseDetails(caseNumber).subscribe(
-        data => this.caseDetails = data,
-        error => console.error('Error fetching case details', error)
+        (data) => (this.caseDetails = data),
+        (error) => console.error('Error fetching case details', error),
       );
     }
   }
